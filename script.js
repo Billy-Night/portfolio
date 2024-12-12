b40624a82e151837bd4f8c9748c2cebffbe0bc23
Billy <billy.m.nightingale@gmail.com>
2024-12-12 17:42:12 +0100

    added connection between HTML and JS files, added functionality to learn more btn so smooth scrolling is operational from intro to the top of projects sections

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+//~ Smooth scrolling from intro to project sections
+
+//* First select the elements and store them :
+//* The button :
+const btnScrollTo = document.querySelector("#btn-learn-more");
+
+//* The section where the viewport will scroll to :
+const section1 = document.querySelector(".section-project-container");
+
+//* Then add the event onClick
+btnScrollTo.addEventListener("click", function (e) {
+  //* The more modern way, only supported in modern browsers
+  section1.scrollIntoView({ behavior: "smooth" });
+});
