32f5de772a07696986ce0fb16b33a9b9cfbaf75a
Billy <billy.m.nightingale@gmail.com>
2025-02-27 14:39:27 +0100

    Updated component tab to contain some marketing on what I offer, add JS functionality and all is working well

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ btnScrollTo.addEventListener("click", function (e) {
   //* The more modern way, only supported in modern browsers
   section1.scrollIntoView({ behavior: "smooth" });
 });
+
+//~ Tabbed Component
+//* Tabbed Component
+const tabs = document.querySelectorAll(".offers__tab");
+const tabsContainer = document.querySelector(".offers__tab-container");
+const tabsContent = document.querySelectorAll(".offers__content");
+
+//* Event delegations
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".offers__tab");
+  console.log(clicked);
+  //* Guard clause, if return gets executed no code will run after
+  if (!clicked) return;
+
+  //* Remove active classes
+  tabs.forEach((t) => t.classList.remove("offers__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("offers__content--active"));
+  clicked.classList.add("offers__tab--active");
+
+  //* Activate content area
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.offers__content--${clicked.dataset.tab}`)
+    .classList.add("offers__content--active");
+});
