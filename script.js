f435c4904face40a851ea5f5b89df3ac11887ba5
Billy <billy.m.nightingale@gmail.com>
2025-02-27 17:43:12 +0100

    added new fade feature to Navbar, cleaned up JS file by orgainising all the DOM selectos and add checks to other functions as the JS is also used on the about page, adjusted picture on about page, about page needs some changes, added to log

--- script.js
+++ script.js
@@ -1,41 +1,67 @@
 "use strict";
 
-//~ Smooth scrolling from intro to project sections
+//console.log("Script loaded");
 
-//* Select the elements and store them :
-//* Select the button :
-const btnScrollTo = document.querySelector("#btn-learn-more");
+//~ Selected DOM Elements
 
-//* Select the section where the viewport will scroll to :
-const section1 = document.querySelector(".section-project-container");
-
-//* Then add the event onClick
-btnScrollTo.addEventListener("click", function (e) {
-  //* The more modern way, only supported in modern browsers
-  section1.scrollIntoView({ behavior: "smooth" });
-});
-
-//~ Tabbed Component
-//* Tabbed Component
+//* Tabbed Components
 const tabs = document.querySelectorAll(".offers__tab");
 const tabsContainer = document.querySelector(".offers__tab-container");
 const tabsContent = document.querySelectorAll(".offers__content");
+//* Select the button :
+const btnScrollTo = document.querySelector("#btn-learn-more");
+//* Select the section where the viewport will scroll to :
+const section1 = document.querySelector(".section-project-container");
+
+const nav = document.querySelector(".nav");
+
+//~ Smooth scrolling from intro to project sections
+
+//* Then add the event onClick
+if (btnScrollTo) {
+  btnScrollTo.addEventListener("click", function (e) {
+    //* The more modern way, only supported in modern browsers
+    section1.scrollIntoView({ behavior: "smooth" });
+  });
+}
 
 //* Event delegations
-tabsContainer.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".offers__tab");
-  console.log(clicked);
-  //* Guard clause, if return gets executed no code will run after
-  if (!clicked) return;
+if (tabsContainer) {
+  tabsContainer.addEventListener("click", function (e) {
+    const clicked = e.target.closest(".offers__tab");
+    //console.log(clicked);
+    //* Guard clause, if return gets executed no code will run after
+    if (!clicked) return;
 
-  //* Remove active classes
-  tabs.forEach((t) => t.classList.remove("offers__tab--active"));
-  tabsContent.forEach((c) => c.classList.remove("offers__content--active"));
-  clicked.classList.add("offers__tab--active");
+    //* Remove active classes
+    tabs.forEach((t) => t.classList.remove("offers__tab--active"));
+    tabsContent.forEach((c) => c.classList.remove("offers__content--active"));
+    clicked.classList.add("offers__tab--active");
 
-  //* Activate content area
-  console.log(clicked.dataset.tab);
-  document
-    .querySelector(`.offers__content--${clicked.dataset.tab}`)
-    .classList.add("offers__content--active");
-});
+    //* Activate content area
+    console.log(clicked.dataset.tab);
+    document
+      .querySelector(`.offers__content--${clicked.dataset.tab}`)
+      .classList.add("offers__content--active");
+  });
+}
+
+// Menu fade animation
+const handlerHover = function (e) {
+  //console.log(this);
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    //console.log(link);
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", handlerHover.bind(0.5));
+
+nav.addEventListener("mouseout", handlerHover.bind(1));
